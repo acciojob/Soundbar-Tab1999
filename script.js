9604e65342fbc42c309c93f0f9696dca7c711ab2
Tab1999 <103740095+Tab1999@users.noreply.github.com>
2025-08-26 09:40:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+const buttons = document.querySelectorAll('.btn');
+
+buttons.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    if (btn.classList.contains('stop')) {
+      stopSounds();
+    } else {
+      playSound(btn.innerText);
+    }
+  });
+});
+
+function playSound(sound) {
+  stopSounds();
+  const audio = new Audio(`sounds/${sound}.mp3`);
+  audio.play();
+  currentAudio = audio; 
+}
+
+let currentAudio = null;
+
+function stopSounds() {
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+    currentAudio = null;
+  }
+}
